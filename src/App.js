87579d43b5af12a71315f439f994c7bf1b99631e
Aydin <iranica@gmail.com>
2021-04-06 18:16:46 +0200

    Our first Blog

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Client from 'client';
 import './App.css';
+
+const [post, setPost] = useState([]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header>
+          <div className="wrapper">
+            <span>React and Contentful</span>
+          </div>
+        </header>
+        <main>
+          <div className="wrapper">
+            
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
